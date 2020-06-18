e2abc7f85215e1710a45acb44806e389b35f181c
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-06-18 15:53:10 -0500

    add heartbeat

--- worker/queueArgyle.js
+++ worker/queueArgyle.js
@@ -14,7 +14,7 @@ exports.start = function (apiKey, channelName, queueName, queueEndpoint) {
 
   console.log('ably ', apiKey, channelName, queue, queueEndpoint)
 
-  amqp.connect(url, (err, conn) => {
+  amqp.connect(url + '?heartbeat=60', (err, conn) => {
     if (err) {
       return console.error('worker:', 'Queue error!', err)
     }
