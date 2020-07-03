79b17b87a944cf7d5a669150c6d4b4c5ee17ce97
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-07-03 11:29:22 -0500

    cookie sesion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const logger = require('morgan')
 const path = require('path')
 const cors = require('cors')
-const session = require('express-session')
+const session = require('cookie-session')
 const passport = require('./config/passport')
 const compression = require('compression')
 
@@ -23,8 +23,8 @@ const worker = require('./worker/queueArgyle')
 //worker.start('W7Josg.fLykxw:lZzlEJw-VacfxEX3', 'GA:payments', 'GA', 'us-east-1-a-queue.ably.io:5671/shared')
 
 mongoose
-    .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
 
 const app_name = require('./package.json').name
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
@@ -32,19 +32,19 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express()
 
 app.use(
-    cors({
-        credentials: true,
-        origin: [process.env.FRONTENDPOINT, process.env.REDIRECT_ENDPOINT],
-    })
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTENDPOINT, process.env.REDIRECT_ENDPOINT],
+  })
 )
 
 app.use(
-    session({
-        resave: false,
-        saveUninitialized: true,
-        secret: process.env.SECRET,
-        cookie: { maxAge: 1000 * 60 * 60 },
-    })
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
 )
 
 app.use(passport.initialize())
--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,71 +1,66 @@
-const crypto = require('crypto');
-require('dotenv').config();
+const crypto = require('crypto')
+require('dotenv').config()
 const User = require('../models/User')
 
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 const URL = {
-    staging: 'https://gaweb.netlify.app',
-    local: 'http://localhost:3001',
-    prod: 'https://green-acorn.netlify.app',
-    prodR: 'https://green-acorn-app.netlify.app'
+  staging: 'https://gaweb.netlify.app',
+  local: 'http://localhost:3001',
+  prod: 'https://green-acorn.netlify.app',
+  prodR: 'https://green-acorn-app.netlify.app',
 }
-module.exports = (app) => {
-    app.post('/forgotpassword', (req, res) => {
-        console.log(req.body.email)
-        if (req.body.email === '') {
-            res.status(400).send('email required');
-        }
-        console.error(req.body.email);
-        User.findOne({ email: req.body.email }, ).then((user) => {
-            if (user === null) {
-                console.error('email not in database');
-                res.status(403).send('email not in db');
-            } else {
-                const token = crypto.randomBytes(20).toString('hex')
+module.exports = app => {
+  app.post('/forgotpassword', (req, res) => {
+    console.log(req.body.email)
+    if (req.body.email === '') {
+      res.status(400).send('email required')
+    }
+    console.error(req.body.email)
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user === null) {
+        console.error('email not in database')
+        res.status(403).send('email not in db')
+      } else {
+        const token = crypto.randomBytes(20).toString('hex')
 
+        user.resetPasswordToken = token
+        user.resetPasswordExpires = Date.now() + 3600000
+        user.save(function (err) {
+          if (err) {
+            console.error('ERROR!')
+          }
+        })
 
-                user.resetPasswordToken = token
-                user.resetPasswordExpires = Date.now() + 3600000
-                user.save(function(err) {
-                    if (err) {
-                        console.error('ERROR!');
-                    }
-                });
+        const transporter = nodemailer.createTransport({
+          service: 'SendGrid',
+          auth: {
+            user: process.env.SGUSER,
+            pass: process.env.SGPASSWORD,
+          },
+        })
 
-
-
-
-
-                const transporter = nodemailer.createTransport({
-                    service: 'SendGrid',
-                    auth: {
-                        user: process.env.SGUSER,
-                        pass: process.env.SGPASSWORD
-                    },
-                });
-
-                const mailOptions = {
-                    from: 'info@greenacorn.com',
-                    to: `${user.email}`,
-                    subject: 'Reset Password',
-                    html: `You are receiving this because you (or someone else) have requested the reset of the password for your account. <br/>
+        const mailOptions = {
+          from: 'info@greenacorn.com',
+          to: `${user.email}`,
+          subject: 'Reset Password',
+          html: `You are receiving this because you (or someone else) have requested the reset of the password for your account. <br/>
                            Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it: <br/> 
-                        <a href="${URL.prodR}/reset/${token}"> Click here</a> <br/>
+                        <a href="${URL.staging}/reset/${token}"> Click here</a> <br/>
                         If you did not request this, please ignore this email and your password will remain unchanged.`,
-                };
+        }
 
-                console.log('sending mail');
+        console.log('sending mail')
 
-                transporter.sendMail(mailOptions, (err, response) => {
-                    if (err) {
-                        console.error('there was an error: ', err);
-                    } else {
-                        console.log('here is the res: ', response);
-                        res.status(200).json('recovery email sent');
-                    }
-                });
-            }
-        });
-    });
-};
+        transporter.sendMail(mailOptions, (err, response) => {
+          if (err) {
+            console.error('there was an error: ', err)
+          } else {
+            console.log('here is the res: ', response)
+            res.status(200).json('recovery email sent')
+          }
+        })
+      }
+    })
+  })
+}
