8ab56f566c950d58b2c4ad02bab5d210bc8c46dd
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-07-16 10:33:55 -0500

    add service for update time

--- routes/estimate.js
+++ routes/estimate.js
@@ -1,20 +1,51 @@
 const { Router } = require('express')
 const router = Router()
-const { createEstimate, deleteWorker, filterDate, createJob, getJobsClose, getJobsOpen, updateExpense, closeJob, sendInvoice2, sendEstimate, updateInvoice, deleteExpense, acceptPayment, deleteInvoice, createInvoice, addPM, oneJob, addTime, addWorkers, paidInvoice, estimateUpdate, deleteAll, getOneEstimate, getAllEstimates, getAllInvoices, getAllJobs, convertInvoice, convertJob, decline, addExpense, addArgyleCharge } = require('../controllers/estimateControllers')
+const {
+  createEstimate,
+  deleteWorker,
+  filterDate,
+  createJob,
+  getJobsClose,
+  getJobsOpen,
+  updateExpense,
+  closeJob,
+  sendInvoice2,
+  sendEstimate,
+  updateInvoice,
+  deleteExpense,
+  acceptPayment,
+  deleteInvoice,
+  createInvoice,
+  addPM,
+  oneJob,
+  addTime,
+  addWorkers,
+  paidInvoice,
+  estimateUpdate,
+  deleteAll,
+  getOneEstimate,
+  getAllEstimates,
+  getAllInvoices,
+  getAllJobs,
+  convertInvoice,
+  convertJob,
+  decline,
+  addExpense,
+  addArgyleCharge,
+  updateTime,
+} = require('../controllers/estimateControllers')
 
-
-
-router.post('/addestimate', createEstimate);
-router.post('/createjob', createJob);
-router.get('/checkestimates', getAllEstimates);
-router.get('/checkjobs', getAllJobs);
-router.get('/openjobs', getJobsOpen);
+router.post('/addestimate', createEstimate)
+router.post('/createjob', createJob)
+router.get('/checkestimates', getAllEstimates)
+router.get('/checkjobs', getAllJobs)
+router.get('/openjobs', getJobsOpen)
 router.post('/filterdate', filterDate)
-router.get('/closejobs', getJobsClose);
-router.patch('/closejob/:id', closeJob);
-router.patch('/convertinvoice/:id', createInvoice);
-router.get('/convertjob/:id', convertJob);
-router.patch('/estimatedecline/:id', decline);
+router.get('/closejobs', getJobsClose)
+router.patch('/closejob/:id', closeJob)
+router.patch('/convertinvoice/:id', createInvoice)
+router.get('/convertjob/:id', convertJob)
+router.patch('/estimatedecline/:id', decline)
 router.delete('/estimatedelete/:id', deleteAll)
 router.patch('/invoicedelete/:estimateId/:id', deleteInvoice)
 router.patch('/invoiceupdate/:estimateId/:invoiceId', updateInvoice)
@@ -22,14 +53,15 @@ router.patch('/expenseupdate/:estimateId/:expenseId', updateExpense)
 router.patch('/expensedelete/:estimateId/:expenseId', deleteExpense)
 router.patch('/workerdelete/:estimateId/:workerId', deleteWorker)
 router.patch('/addexpense/:id', addExpense)
-router.post('/sendestimate', sendEstimate);
-router.post('/sendinvoice', sendInvoice2);
-router.get('/estimatedetail/:id', getOneEstimate);
+router.post('/sendestimate', sendEstimate)
+router.post('/sendinvoice', sendInvoice2)
+router.get('/estimatedetail/:id', getOneEstimate)
 router.patch('/estimateupdate/:id', estimateUpdate)
 router.patch('/paidinvoice/:id', paidInvoice)
 router.patch('/addworkers/:id', addWorkers)
 router.patch('/addpm/:id', addPM)
 router.patch('/addtime/:id/:workerId', addTime)
+router.patch('/updatetime/:estimateId/:timeId', updateTime)
 router.patch('/pay-invoice/:id/:invoiceId', acceptPayment)
 
-module.exports = router;
+module.exports = router
