62b2e6835b564c2881bcda098cc308e13757234b
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-07-16 15:34:53 -0500

    push 16 jul

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,90 +3,90 @@ const Estimate = require('../models/Estimate')
 const { createToken, createTokenU } = require('../config/jwt')
 const { sendEmail } = require('../config/nodemailer')
 
-
 exports.signup = (req, res, next) => {
-    User.register({...req.body }, req.body.password)
-        .then(user => { res.status(201).json({ user }) })
-        .catch(err => res.status(500).json({ err }))
+  User.register({ ...req.body }, req.body.password)
+    .then(user => {
+      res.status(201).json({ user })
+    })
+    .catch(err => res.status(500).json({ err }))
 }
 exports.createUser = (req, res, next) => {
-    const { name, email, msg, password } = req.body
-    User.register({...req.body }, req.body.password)
-        .then(user => {
-            sendEmail(email, name, msg, password)
-                .then(info => {
-                    res.status(200)
-                })
-                .catch(err => {
-
-                    res.send(err)
-                })
-            res.status(201).json({ user })
+  const { name, email, msg, password } = req.body
+  User.register({ ...req.body }, req.body.password)
+    .then(user => {
+      sendEmail(email, name, msg, password)
+        .then(info => {
+          res.status(200)
         })
-        .catch(err => res.status(500).json({ err }))
+        .catch(err => {
+          res.send(err)
+        })
+      res.status(201).json({ user })
+    })
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.login = (req, res, next) => {
-    const { user } = req
-    const [header, payload, signature] = createToken(user)
-    res.cookie('headload', `${header}.${payload}.`, {
-        maxAge: 1000 * 60 * 30,
-        secure: true
-    })
-    res.cookie('signature', signature, {
-        httpOnly: true,
-        secure: true
-    })
-    res.status(200).json({ user })
+  const { user } = req
+  const [header, payload, signature] = createToken(user)
+  res.cookie('headload', `${header}.${payload}.`, {
+    maxAge: 1000 * 60 * 30,
+    secure: true,
+  })
+  res.cookie('signature', signature, {
+    httpOnly: true,
+    secure: true,
+  })
+  res.status(200).json({ user })
 }
 exports.logout = (req, res, next) => {
-    res.clearCookie('headload')
-    res.clearCookie('signature')
-    res.status(200).json({ msg: 'Logged out' })
+  res.clearCookie('headload')
+  res.clearCookie('signature')
+  res.status(200).json({ msg: 'Logged out' })
 }
 
 exports.getAllUsers = (req, res, next) => {
-    User.find()
-        .then(users => res.status(200).json({ users }))
-        .catch(err => res.status(500).json({ err }))
+  User.find()
+    .then(users => res.status(200).json({ users }))
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.workerUsers = (req, res, next) => {
-
-    User.find({ role: { $in: ["WORKER", "PROJECT MANAGER"] } }).populate({
-            path: 'works.workId',
-            select: 'expenses jobName dateStart dateEnd'
-        })
-        .then(users => {
-            res.status(200).json({ users })
-        })
-        .catch(err => res.status(500).json({ err }))
+  User.find({ role: { $in: ['WORKER', 'PROJECT MANAGER', 'ADMIN'] } })
+    .populate({
+      path: 'works.workId',
+      select: 'expenses jobName dateStart dateEnd',
+    })
+    .then(users => {
+      res.status(200).json({ users })
+    })
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.pmUsers = (req, res, next) => {
-    User.find({ role: 'PROJECT MANAGER' })
-        .then(users => res.status(200).json({ users }))
-        .catch(err => res.status(500).json({ err }))
+  User.find({ role: 'PROJECT MANAGER' })
+    .then(users => res.status(200).json({ users }))
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.oneWorker = (req, res, next) => {
-    const { id } = req.params
-    User.findById(id)
-        .then(user => res.status(200).json({ user }))
-        .catch(err => res.status(500).json({ err }))
+  const { id } = req.params
+  User.findById(id)
+    .then(user => res.status(200).json({ user }))
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.updateWorker = (req, res, next) => {
-    const { id } = req.params
-    User.findByIdAndUpdate(id, {...req.body }, { new: true })
-        .then(user => res.status(200).json({ user }))
-        .catch(err => res.status(500).json({ err }))
+  const { id } = req.params
+  User.findByIdAndUpdate(id, { ...req.body }, { new: true })
+    .then(user => res.status(200).json({ user }))
+    .catch(err => res.status(500).json({ err }))
 }
 
 exports.deleteWorker = (req, res, next) => {
-    const { id } = req.params
-    console.log(id)
-    User.findByIdAndDelete(id)
-        .then(user => res.status(200).json({ user }))
-        .catch(err => res.status(500).json({ err }))
+  const { id } = req.params
+  console.log(id)
+  User.findByIdAndDelete(id)
+    .then(user => res.status(200).json({ user }))
+    .catch(err => res.status(500).json({ err }))
 }
