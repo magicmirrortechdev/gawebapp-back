6250b7ea97bbf22a4413c7b54412d552a4e0ea77
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-08-23 09:49:44 -0500

    fix addProjectManagers

--- controllers/estimateControllers.js
+++ controllers/estimateControllers.js
@@ -282,11 +282,12 @@ exports.addPM = (req, res, next) => {
   const { id } = req.params
   const { id2 } = req.body
   Estimate.findByIdAndUpdate(id, { $push: { projectManager: { projectId: id2 } } }, { new: true })
-    .then(estimate =>
+    .then(async estimateRes => {
+      const estimate = await Estimate.findById(estimateRes._id)
       User.findByIdAndUpdate(id2, { $push: { works: { workId: id } } }, { new: true })
         .then(user => res.status(200).json({ estimate, user }))
         .catch(err => res.status(500).json({ err }))
-    )
+    })
     .catch(err => res.status(500).json({ err }))
 }
 
