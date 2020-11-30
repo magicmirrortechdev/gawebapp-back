54c5bb2ab32b0f85ae6ce4485983b5c070dd8729
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2020-11-30 13:33:29 -0600

    add times routes

--- v2/routes/TimeRoute.js
+++ v2/routes/TimeRoute.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const router = Router()
-const { getAllTimes, createTime, updateTime, deleteTime } = require('../controllers/TimeControllers')
+const { getAllTimes, createTime, updateTime, deleteTime, getOneTime } = require('../controllers/TimeControllers')
 
 router.get('/gettimes/', getAllTimes)
+router.get('/gettimes/:id', getOneTime)
 router.patch('/addtime/:id/:workerId', createTime)
 router.patch('/updatetime/:estimateId/:workerId/:timeId', updateTime)
 router.patch('/deletetime/:estimateId/:workerId/:timeId', deleteTime)
