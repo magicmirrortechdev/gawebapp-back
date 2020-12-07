6dd9041de747d5fd3650106f622c408745d63c92
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-12-06 22:08:04 -0600

    update expense module

--- v2/controllers/ExpenseControllers.js
+++ v2/controllers/ExpenseControllers.js
@@ -23,24 +23,6 @@ exports.getAllExpenses = async (req, res, next) => {
     .catch(err => res.status(500).json({ err }))
 }
 
-exports.addExpense = async (req, res, next) => {
-  const { id } = req.params
-  const { date, vendor, category, description, img, total, workerId } = req.body
-
-  const estimate = await Estimate.findByIdAndUpdate(
-    id,
-    { $push: { expenses: { date, vendor, category, description, img, total, workerId } } },
-    { new: true }
-  )
-    .populate('clientId')
-    .populate({ path: 'workerId' })
-  Expense.create(workerId, {
-    $push: { expenses: { jobName: estimate.jobName, date, vendor, category, description, img, total } },
-  })
-    .then(user => res.status(200).json({ estimate, user }))
-    .catch(err => res.status(500).json({ err }))
-}
-
 exports.updateExpense = async (req, res, next) => {
   const { id } = req.params
   Expense.findByIdAndUpdate(id, { ...req.body }, { new: true })
--- v2/routes/ExpenseRoute.js
+++ v2/routes/ExpenseRoute.js
@@ -1,9 +1,9 @@
 const { Router } = require('express')
 const router = Router()
-const { getAllExpenses, addExpense, updateExpense, deleteExpense } = require('../controllers/ExpenseControllers')
+const { getAllExpenses, createExpense, updateExpense, deleteExpense } = require('../controllers/ExpenseControllers')
 
 router.get('/getexpenses/:id', getAllExpenses)
-router.patch('/addexpense/:id', addExpense)
+router.patch('/addexpense/', createExpense)
 router.patch('/expenseupdate/:estimateId/:expenseId', updateExpense)
 router.patch('/expensedelete/:estimateId/:expenseId', deleteExpense)
 
